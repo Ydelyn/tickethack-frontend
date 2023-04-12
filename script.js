498946ef3ff70031e09018b52c821d4657d4203d
Samuel Javelle <samueljavelle@hotmail.com>
2023-04-12 09:56:05 +0200

    fix multi-search

--- script.js
+++ script.js
@@ -22,11 +22,13 @@ document.querySelector('#input-submit').addEventListener('click', function(){
         })
         .then(resp => resp.json())
         .then(data => {
+            document.querySelector('#result').innerHTML = ''
             if(data.trips.length === 0){
-                document.querySelector('#result').firstElementChild.src = "./img/notfound.png"
-                document.querySelector('#result').lastElementChild.textContent = "No trip found." 
+                document.querySelector('#result').innerHTML = `
+                <img src="./img/notfound.png" alt="Image de train" class="m-auto w-1/2">
+                <hr class="w-3/4 mx-auto my-6 h-0.5 bg-[#4FAA91]">
+                <p class="">No trip found.</p>`
             }else{
-                document.querySelector('#result').innerHTML = ''
                 for(trip of data.trips){
                     document.querySelector('#result').innerHTML +=`
                     <div class="flex">
